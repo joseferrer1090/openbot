b964f941b1de7eb96c730a3a108a07f76ec2cc0a
Jose Carlos <jcfb90@gmail.com>
2017-09-05 13:12:33 -0500

    Footer y demas secciones de la pagina

--- src/Components/Prinicipal/principal.js
+++ src/Components/Prinicipal/principal.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 import Header from "../../Layouts/header";
 import Slider from "../../Layouts/slider";
+import Footer from "../../Layouts/footer";
+import imgOpenBot from './../../Layouts/img/Recurso2.png';
+
 
 class Principal extends Component{
     render(){
@@ -9,6 +12,30 @@ class Principal extends Component{
             <div>
                 <Header/>
                 <Slider/>
+                <section className="We-Are-Popular">
+                    <div className="container">
+                        <h2>We Are Popular Everywhere</h2>
+                        <h6 className="text-justify">Build your team around your business structure, Often a business builds its structure around<br/> people rather than people around structures. This leads to the position where loyal people<br/> end up in roles that they are not suitable for</h6>
+                        <div className="row">
+                            <div className="col-md-8 popular-map">
+                                <img src={imgOpenBot} alt="" className="img-responsive"/>
+                            </div>
+                            <div className="col-md-4 popover-content">
+                                <div className="popular-text">
+                                    <h5>First One</h5>
+                                    <p>Doing some hard thinking about the context that your business inhabits in order to protect it against what's around the corner, shouldn't feel overwhelming</p>
+                                    <a href="about-us.html" className="">More Explore&nbsp;&nbsp;<i className="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                                <div className="popular-text">
+                                    <h5>First One</h5>
+                                    <p>Doing some hard thinking about the context that your business inhabits in order to protect it against what's around the corner, shouldn't feel overwhelming</p>
+                                    <a href="about-vs2.html" className="">More Explore&nbsp;&nbsp;<i className="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <Footer/>
             </div>
         )
 
--- src/Layouts/footer.js
+++ src/Layouts/footer.js
@@ -6,7 +6,77 @@ class Footer extends Component {
     render (){
         return(
             <div>
-                
+                <footer>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-4 col-sm-4 col-xs-12">
+                                <div className="footer-logo">
+                                    <div className="logo"><a href="index.html"><img src="images/logo/logo.png" alt="Logo" /></a></div>
+                                    <h6>b core was established in 2017 by talented business veterans, Rod Davies and Matthew Levington, the journey began when founders Matthew Levington and Rod Davies, met up in 2017 over a beer</h6>
+                                    <ul className="footer-icon">
+                                        <li><a href="#" className="round-border hvr-shutter-out-horizontal-three"><i className="fa fa-facebook" aria-hidden="true" /></a></li>
+                                        <li><a href="#" className="round-border hvr-shutter-out-horizontal-three"><i className="fa fa-linkedin" aria-hidden="true" /></a></li>
+                                        <li><a href="#" className="round-border hvr-shutter-out-horizontal-three"><i className="fa fa-twitter" aria-hidden="true" /></a></li>
+                                        <li><a href="#" className="round-border hvr-shutter-out-horizontal-three"><i className="fa fa-dribbble" aria-hidden="true" /></a></li>
+                                    </ul>
+                                    <ul className="footer-featured">
+                                        <li><a href="news-details.html" className>Privacy Policy</a></li>
+                                        <li><a href="news-details.html" className>Legal Policy</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-4 col-xs-12">
+                                <div className="footer-Useful-Links">
+                                    <h5>Useful Links</h5>
+                                    <ul>
+                                        <li><a href="index.html" className>Home</a></li>
+                                        <li><a href="about-us.html" className>About us </a></li>
+                                        <li><a href="project.html" className>Project</a></li>
+                                        <li><a href="our-team.html" className>Advisors</a></li>
+                                        <li><a href="service.html" className>Performance</a></li>
+                                        <li><a href="blog.html" className>News</a></li>
+                                        <li><a href="appointment-form.html" className>Conditions</a></li>
+                                    </ul>
+                                    <ul>
+                                        <li><a href="contact-us.html" className>About us</a></li>
+                                        <li><a href="service-singel.html" className>Our History</a></li>
+                                        <li><a href="news-v2.html" className>Case Studies</a></li>
+                                        <li><a href="blog-single.html" className>Events</a></li>
+                                        <li><a href="project-single.html" className>Business Growth</a></li>
+                                        <li><a href="contact.html" className>Contact us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-4 col-xs-12">
+                                <div className="footer-Update">
+                                    <h5>News Update</h5>
+                                    <div className="footer-News">
+                                        <h6><a href="#" className>The Original Business Doctors<br /> support to business</a></h6>
+                                        <p>january 02,2017</p>
+                                    </div>
+                                    <div className="footer-News">
+                                        <h6><a href="#" className>On Life Goals and Financial Priorities<br /> Cutting Risks in Forex</a></h6>
+                                        <p>january 02,2017</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bottom-footer">
+                        <div className="container">
+                            <div className="float-left">
+                                <p>Copyright ©2017 <a href="https://themeforest.net/user/designolo/portfolio" className="tran3s p-color" target="_blank">bcore</a> All Rights Reserved by <a href="https://themeforest.net/user/designolo/portfolio" className="p-color tran3s" target="_blank">ollo</a></p>
+                            </div> {/* /.float-left */}
+                            <div className="float-right">
+                                <form action="#">
+                                    <input type="text" placeholder="Your email" />
+                                    <button className="hvr-shutter-out-horizontal">Subscribe</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
         )
     }
